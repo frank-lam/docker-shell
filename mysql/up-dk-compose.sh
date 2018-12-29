0fbb05b7e846974c66d446e968831e73f3c387b2
Frank <1002039945@qq.com>
2018-12-29 23:25:02 +0800

    no message

--- mysql/up-dk-compose.sh
+++ mysql/up-dk-compose.sh
@@ -7,3 +7,4 @@ sourcepath="/DATA_DIR"
 basepath=$(cd `dirname $0`; pwd)
 sed -i "s|\/DATA_DIR|$basepath|g" docker-compose-tmp.yml
 mv docker-compose-tmp.yml docker-compose.yml
+rm -f up-dk-compose.sh
