f6535cc4c637a36ef440ed1ce54ea8cf925d0a61
Frank <1002039945@qq.com>
2018-12-30 00:28:27 +0800

    no message

--- lanproxy/upshell.sh
+++ lanproxy/upshell.sh
@@ -3,3 +3,4 @@ wget https://raw.githubusercontent.com/frank-lam/docker-shell/master/lanproxy/re
 wget https://raw.githubusercontent.com/frank-lam/docker-shell/master/lanproxy/start.sh
 wget https://raw.githubusercontent.com/frank-lam/docker-shell/master/lanproxy/stop.sh
 wget https://raw.githubusercontent.com/frank-lam/docker-shell/master/lanproxy/status.sh
+rm upshell.sh
